6cd0895079f2aeb48caacdb9d402afd3c1f9bc3f
App User <user@local.app>
2026-01-26 15:06:26 +0300

    Enhance Appointment features: Add Types, Reminders, and Layout update

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'arac-takip-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    'https://unpkg.com/react@18/umd/react.development.js',
+    'https://unpkg.com/react-dom@18/umd/react-dom.development.js',
+    'https://unpkg.com/@babel/standalone/babel.min.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
